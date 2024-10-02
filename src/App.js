4a9bd5a6e2189f22b08794bbba857471dc1bd47b
Dhvani Mistry <dkm12305@gmail.com>
2024-10-02 21:56:44 +0530

    login_form_components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { LoginForm } from './Components/LoginForm/LoginForm';
 
 function App() {
   return (
     <div>
-      
+      <LoginForm/>
     </div>
   );
 }
